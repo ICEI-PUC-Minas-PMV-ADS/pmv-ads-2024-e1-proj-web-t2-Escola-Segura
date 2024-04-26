c90b356bf306538f7f7480c60fa3c925542f716d
Lucas Rangel <155129810+lucasrangelps@users.noreply.github.com>
2024-04-26 00:16:30 -0300

    correcao de bug login
    corrigido bug pagina login quando acessado por usuario sem cadastro

--- src/paginaLogin/scriptLogin.js
+++ src/paginaLogin/scriptLogin.js
@@ -12,24 +12,30 @@ function logar() {
     let dadosUsuario = [];
 
     dadosUsuario = JSON.parse(localStorage.getItem("dadosUsuario"));
-
-  
-    for (let i = 0; i < dadosUsuario.length; i++) {
-        const elemento = dadosUsuario[i];
-        
-        if (emailUsu.value == elemento.emailUsuario && senhaUsu.value == elemento.senhaUsuario);
-
-        tempUsuario = {
-            email: elemento.emailUsuario,
-            senha: elemento.senhaUsuario
-         }
-  
-
-    }
     
-        if (email.value == tempUsuario.email && senha.value == tempUsuario.senha ){
-            window.location.href = "../paginaHome/index.html";
-        } else {
-            alert("Usuario ou senha Invalidos!")
+
+    //Verifica se possui dados armazenados, se vazio não entra no for e exibe alert
+    if (dadosUsuario != null) {
+
+        for (let i = 0; i < dadosUsuario.length; i++) {
+            const elemento = dadosUsuario[i];
+            
+            if (emailUsu.value == elemento.emailUsuario && senhaUsu.value == elemento.senhaUsuario);
+
+            tempUsuario = {
+                email: elemento.emailUsuario,
+                senha: elemento.senhaUsuario
+            }
+    
+
         }
+        
+            if (email.value == tempUsuario.email && senha.value == tempUsuario.senha ){
+                window.location.href = "../paginaHome/index.html";
+            } else {
+                alert("Usuario ou senha Invalidos!")
+            }
+    } else {
+        alert("Usuario não Cadastrado!")
+    }
 }
