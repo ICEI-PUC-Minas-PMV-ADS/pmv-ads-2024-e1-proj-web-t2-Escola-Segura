799967a6d14a621e843941bf28504d9f03ad87d9
Lucas Rangel <155129810+lucasrangelps@users.noreply.github.com>
2024-05-08 23:23:17 -0300

    update pagina certificado
    atualizado metodo para gerar impressao compativel em todos os navegadores e locais

--- src/paginaCertificado/scriptCertificado.js
+++ src/paginaCertificado/scriptCertificado.js
@@ -1,19 +1,7 @@
-const btnGenerate = document.querySelector("#generatePdf");
+function gerarpdf() { 
 
-btnGenerate.addEventListener("click", () => {
+    $('.printable').html();
+    window.print();
 
-    const content = document.querySelector("#content");
+}
 
-    const options = {
-
-        margin: [0, 0, 0, 2],
-        filename: "certficado.pdf",
-        html2canvas: { scale: 2 },
-        jsPDF: { unit: "mm", format: "a4", putOnlyUsedFonts:true, orientation: "landscape" },
-    };
-
-    html2pdf().set(options).from(content).save();
-
-    console.log("OK");
-    return;
-});
